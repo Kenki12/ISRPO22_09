5501bf8f9dd4050b4f062afc6bca43f135d40f3f
retor <retor@MacBook-Air-retor.local>
2026-09-22 16:38:26 +0300

    Внес изменения в фаил main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import operator
+import turtle as t
+print("Калькулятор")
+f = input("Графики. 1 - прямая, 2 - гипербола")
+if f == "1":
+    print("1")
+elif f == "2":
+    print("2")
+elif f == "3":
+    operations = {
+    "+": operator.add,
+    "-": operator.sub,
+    "*": operator.mul,
+    "/": operator.truediv,
+    "//":operator.floordiv,
+    "%": operator.mod,
+    "**": operator.pow
+    }
+    a, op, b = input("Введите выражение: ").split()
+    a = float(a)
+    b = float(b)
+
+    result = operations[op](a, b)
+
+    print(result)
+
+else:
+    print("Ошибка ввода")
